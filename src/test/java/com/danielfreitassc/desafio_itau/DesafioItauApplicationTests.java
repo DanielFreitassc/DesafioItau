aa4daa1b38aec1f6250e6aed50856056bbc37031
DanielFreitassc <danielfreitassc@gmail.com>
2025-01-25 19:15:05 -0300

    First commit

---
+++ src/test/java/com/danielfreitassc/desafio_itau/DesafioItauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielfreitassc.desafio_itau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioItauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
